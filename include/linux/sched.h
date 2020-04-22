29e8a54921489ef9d6e640e9729fc804b12f3c42
Piyush Kumar <piyushkumg@gmail.com>
2020-04-22 20:02:29 +0530

    Revert cpufreq changes
    Apply them after EAS merge.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1476,8 +1476,6 @@ struct task_struct {
 
 	cputime_t utime, stime, utimescaled, stimescaled;
 	cputime_t gtime;
-	atomic64_t *time_in_state;
-	unsigned int max_states;
 	unsigned long long cpu_power;
 #ifndef CONFIG_VIRT_CPU_ACCOUNTING_NATIVE
 	struct cputime prev_cputime;
@@ -1793,8 +1791,6 @@ struct task_struct {
 	unsigned int	sequential_io;
 	unsigned int	sequential_io_avg;
 #endif
-	atomic64_t *concurrent_active_time;
-	atomic64_t *concurrent_policy_time;
 };
 
 /* Future-safe accessor for struct task_struct's cpus_allowed. */
